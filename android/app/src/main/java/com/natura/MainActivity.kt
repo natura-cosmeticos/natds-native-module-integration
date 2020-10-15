6ec4fbb54bf2801da61a2974b1ebec8337cd3121
Marcella Souza <mlsouza@thoughtworks.com>
2020-10-15 15:27:49 -0300

    feat: 🎸 [DSY-1492] Adding rn icons lib

--- android/app/src/main/java/com/natura/MainActivity.kt
+++ android/app/src/main/java/com/natura/MainActivity.kt
@@ -3,9 +3,8 @@ package com.natura
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.rn_sample_android_lib.RNSampleActivity
+import com.example.rnsample.RNSampleActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import net.natura.sellingcartlibrary.SellingCartLibraryActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,10 +17,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         reactNative1button.setOnClickListener {
-            startActivity(Intent(this, SellingCartLibraryActivity::class.java))
-        }
-
-        reactNative2button.setOnClickListener {
             startActivity(Intent(this, RNSampleActivity::class.java))
         }
     }
